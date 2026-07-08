2822610712cf48e703bc864a543ec587c5691a2f
Nelly <nelly.fabre@hotmail.fr>
2026-07-08 16:21:43 +0200

    feat: change location of Bonjour $user

--- figurines_expo/header.php
+++ figurines_expo/header.php
@@ -1,12 +1,6 @@
 <header class="container-fluid d-flex flex-row justify-content-between align-items-center my-5 bg-light py-3">
 
-    <div class="col-4">
-        <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
-            <strong>Bonjour <?= htmlspecialchars($_SESSION['LOGGED_USER']['login']) ?></strong>
-        <?php endif; ?>
 
-        <p class="mb-0">Nous sommes le <?= date("d/m/Y") ?></p>
-    </div>
 
     <div class="col-8 text-end">
         <ul class="list-inline mb-0">
@@ -19,9 +13,9 @@
             <li class="list-inline-item">
                 <button class="btn btn-outline-primary">
                     <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
-                        <a href="Logout.php">Se déconnecter</a>
+                        <a href="index.php">Se déconnecter</a>
                     <?php else : ?>
-                        <a href="Login.php">Se connecter</a>
+                        <a href="login.php">Se connecter</a>
                     <?php endif; ?></button>
             </li>
         </ul>
