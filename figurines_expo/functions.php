c239a0b67d50809e958e6b476c74683316dfa9b7
Nelly <nelly.fabre@hotmail.fr>
2026-07-08 11:41:42 +0200

    feat: add function.php file

---
+++ figurines_expo/functions.php
@@ -0,0 +1,8 @@
+<?php
+function redirectToUrl(string $url): never
+{
+
+    header("Location: {$url}");
+
+    exit();
+}
---
+++ figurines_expo/submit-login.php
@@ -0,0 +1,66 @@
+<?php
+
+session_start();
+
+require(__DIR__ . '/connect.php');
+
+$sqlQuerySell = 'SELECT * FROM `vendeurs`';
+$sellBdd = $mysqlClient->prepare($sqlQuerySell);
+$sellBdd->execute();
+$sellers = $sellBdd->fetchAll();
+
+
+$postData = $_POST;
+
+
+if (isset($postData['email']) && isset($postData['mdp'])) {
+
+
+    if (!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)) {
+
+        $_SESSION['LOGIN_ERROR_MESSAGE'] = 'Merci de soumettre un email valide.';
+    } else {
+
+        foreach ($sellers as $user) {
+
+            if (
+                $user['mail'] === $postData['email'] &&
+                $user['pswd'] === $postData['mdp']
+            ) {
+                /*
+                 * ✅ AUTHENTIFICATION RÉUSSIE
+                 *
+                 * On stocke les informations de l'utilisateur en session
+                 * Attention : On ne stocke JAMAIS le mot de passe en session !
+                 */
+                $_SESSION['LOGGED_USER'] = [
+                    'email' => $user['mail'],
+                    'nom' => $user['nom'],
+                    'prenom' => $user['prenom'],
+                ];
+            }
+        }
+
+
+
+        if (!isset($_SESSION['LOGGED_USER'])) {
+            /*
+             * ❌ ÉCHEC DE L'AUTHENTIFICATION
+             *
+             * Aucun utilisateur ne correspond aux identifiants fournis
+             * On crée un message d'erreur
+             *
+             * sprintf() permet de formater une chaîne avec des variables
+             * strip_tags() retire les balises HTML pour éviter les injections XSS
+             */
+            $_SESSION['LOGIN_ERROR_MESSAGE'] = sprintf(
+                'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
+                $postData['email'],
+                strip_tags($postData['mdp'])
+            );
+        }
+    }
+
+
+    redirectToUrl('Read.php');
+}
